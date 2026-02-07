d90d2dd8137412d458e717592b6bba90c88471af
Sharali-25 <Sharaliarkansas@gmail.com>
2026-02-07 09:10:04 -0600

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate
+# exit()
+
+for i in range(10):
+
+# If the value of i becomes
+	# 5 then the program is forced
+	# to exit
+    if i == 9:
+
+        # print the exit message
+        print(exit)
+        exit()
+    print(i)
